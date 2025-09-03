9c9d45e00228c8ec7590abee0c76f2c735fc5b0f
Thomas Wetmore <ttw4@icloud.com>
2025-09-03 16:01:46 -0400

    Cleanup of path.c

--- DeadEndsLib/Utils/path.c
+++ DeadEndsLib/Utils/path.c
@@ -10,77 +10,42 @@
 
 #define MAXPATHBUFFER 4096
 
-// resolveFile tries to find a file within a sequence of paths.
-String oldResolveFile(String name, String path, String suffix) {
-    if (!name || *name == 0) return null;
-    if (!path || *path == 0) return strsave(name);
-    if (strchr(name, '/') != null) return strsave(name);
-    char buf1[MAXPATHBUFFER];
-    strcpy(buf1, path);
-    char buf2[MAXPATHBUFFER];
-    char* p = strtok(buf1, ":");
-    while (p) {
-        snprintf(buf2, sizeof(buf2), "%s/%s", p, name);
-        if (access(buf2, F_OK) == 0) return strsave(buf2);
-        p = strtok(null, ":");
-    }
-    return null;
-}
-
 /// resolveFile tries to find a file within a colon-separated path list.
-/// If not found, and a suffix is provided, tries again with the suffix appended.
+/// If not found, and a suffix is provided, tries again with suffix appended.
 String resolveFile(String name, String path, String suffix) {
 
     if (!name || *name == 0) return null;  // No file name.
     if (!path || *path == 0) return strsave(name);  // No path.
-    if (strchr(name, '/')) return strsave(name);  // File name is absolute.
-    char buf1[MAXPATHBUFFER];
-    char fullPath[MAXPATHBUFFER];
+    if (*name == '/') return strsave(name);  // Absolute file name.
+    char pathbuf[MAXPATHBUFFER];
+    char fullpath[MAXPATHBUFFER];
 
-    // Search path list for original name
-    strcpy(buf1, path);
-    for (char* dir = strtok(buf1, ":"); dir; dir = strtok(NULL, ":")) {
-        snprintf(fullPath, sizeof(fullPath), "%s/%s", dir, name);
-        if (access(fullPath, F_OK) == 0) return strsave(fullPath);
+    // Search path list for the filename.
+    strcpy(pathbuf, path);
+    for (char* dir = strtok(pathbuf, ":"); dir; dir = strtok(null, ":")) {
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, name);
+        if (access(fullpath, F_OK) == 0) return strsave(fullpath);
     }
-    // Try with suffix if provided
-    if (suffix && *suffix != 0) {
-        strcpy(buf1, path);  // Reset buf1 because strtok modifies it
-        String fmt = (*suffix == '.') ? "%s/%s%s" : "%s/%s.%s";
-        for (char* dir = strtok(buf1, ":"); dir; dir = strtok(NULL, ":")) {
-            snprintf(fullPath, sizeof(fullPath), fmt, dir, name, suffix);
-            if (access(fullPath, F_OK) == 0) return strsave(fullPath);
-        }
+    // Try with suffix if provided.
+    if (!suffix || !*suffix) return null;
+    strcpy(pathbuf, path);  // Reset buf because strtok modifies it
+    String fmt = (*suffix == '.') ? "%s/%s%s" : "%s/%s.%s";
+    for (char* dir = strtok(pathbuf, ":"); dir; dir = strtok(null, ":")) {
+        snprintf(fullpath, sizeof(fullpath), fmt, dir, name, suffix);
+        if (access(fullpath, F_OK) == 0) return strsave(fullpath);
     }
     return null;
 }
 
-// fopenPath attempta to open a file using a path variable.
+/// Tries to open a file using a path variable and optional suffix.
 FILE *fopenPath(String name, String mode, String path, String suffix) {
     String str;
     if (!(str = resolveFile(name, path, suffix))) return null;
     return fopen(str, mode);
 }
 
-// lastPathSegment returns the last componenet of a path. Returns static memory.
-String oldLastPathSegment (String path) {
-    static unsigned char scratch[MAXPATHBUFFER];
-    if (!path || *path == 0) return NULL;
-    int len = (int) strlen(path);
-    String p = (String) scratch, q;
-    strcpy(p, path);
-    if (p[len-1] == '/') {
-        len--;
-        p[len] = 0;
-    }
-    q = p;
-    int c;
-    while ((c = *p++)) {
-        if (c == '/') q = p;
-    }
-    return q;
-}
-String lastPathSegment (String path) {
+/// Returns the last componenet of a path. Returns static memory.
+String lastPathSegment(String path) {
     char *lastSlash = strrchr(path, '/');
     if (lastSlash)
         return (lastSlash + 1);
